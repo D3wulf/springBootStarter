37e32473f619261d06b05ad60f2d68d2bfffda49
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-20 09:16:06 +0200

    primer commit

---
+++ src/main/java/spring/starter/prueba/starter/Saludo.java
@@ -0,0 +1,37 @@
+package spring.starter.prueba.starter;
+
+public class Saludo {
+	private String idioma;
+
+	public Saludo(String idioma) {
+		this.idioma = idioma;
+	}
+	
+	public String mensajeSaludo() {
+		String mensaje= "";
+		switch (idioma) {
+		
+		case "Castellano":
+			mensaje="Bienvenido";
+			break;
+			
+		
+		case "Ingles":
+			mensaje="Welcome";
+			break;
+		
+		
+		case "Italiano":
+			mensaje="Bienvenuto";
+			break;
+		
+		default:
+			mensaje="Bienvenido";
+			break;
+		
+		}
+		return mensaje;
+		
+	}
+
+}
---
+++ src/test/java/spring/starter/prueba/starter/StarterApplicationTests.java
@@ -0,0 +1,16 @@
+/*
+package spring.starter.prueba.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+*/
