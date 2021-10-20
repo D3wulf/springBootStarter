b0b019fdc17bac695d40c36cf99ace82c85bf96a
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-20 11:39:19 +0200

    cambio starter muevo package

---
+++ src/main/java/com/proyecto/springboot/miProyecto/config/SaludoAutoconfigure.java
@@ -0,0 +1,28 @@
+package com.proyecto.springboot.miProyecto.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.*;
+
+import spring.starter.prueba.starter.Saludo;
+
+@Configuration
+@ConditionalOnClass(Saludo.class)
+@EnableConfigurationProperties(SaludoProperties.class)
+public class SaludoAutoconfigure {
+	
+	private final SaludoProperties properties;
+	
+	public SaludoAutoconfigure(SaludoProperties properties) {
+		
+		this.properties= properties;
+	}
+	
+	@Bean
+	public Saludo saludo() {
+		return new Saludo(properties.getIdioma());
+	}
+	
+	
+
+}
